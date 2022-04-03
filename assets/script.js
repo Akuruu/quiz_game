d8456a5669c32b938d7220f38f223fb4884ac2fc
Akuru <anjalismith0529@gmail.com>
2022-04-03 18:08:06 -0400

    created quiz questions

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,15 @@ var quizText = document.querySelector(".quiz");
 var startButton = document.querySelector(".start");
 var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
+//---------------------------------
 var question = document.querySelector(".question");
+var choices = document.querySelector(".choice-text");
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+var availableQuestions = [];
+//-------------------------------------
 var timer;
 var timeLeft;
 var isWin = false;
@@ -60,3 +68,71 @@ function gameLose() {
 }
 
 //--------------------------
+var questions = [
+    {
+        question: "What is the name of Xie Lian's spiritual device?",
+        choice1: "E'ming",
+        choice2: "Ruoye",
+        choice3: "Shi Qingxuan",
+        choice4:"Chenqing",
+        answer: 2
+    },
+    {
+        question: "What is Hua Cheng's title?", 
+        choice1: "Black Water Sinking Ships",
+        choice2: "The White Calamity",
+        choice3: "Crimson Rain Sought Flower",
+        choice4: "Night Touring Green Lantern",
+        answer: 3
+    },
+    {
+        question: "What spiritual weapon does Lan Wangji prefer to use?",
+        choice1: "a Guqin",
+        choice2: "a Flute",
+        choice3: "a Whip",
+        choice4: "a Sword",
+        answer: 1
+    },
+    {
+        question: "What did Wei Wuxian sacrifice for Jiang Cheng?",
+        choice1: "His Golden Core",
+        choice2: "His Right Eye",
+        choice3: "A Beaded Bracelet",
+        choice4: "His Nephew",
+        answer: 1
+    },
+    {
+        question: "What is the name of the Peak that Shen Qingqiu resides over?",
+        choice1: "Qiong Ding Peak",
+        choice2: "Qing Jing Peak",
+        choice3: "An Ding Peak",
+        choice4: "Bai Zhan Peak",
+        answer: 2
+    },
+    {
+        question: "What is the name of the poison Shen Qingqiu was afflicted by?",
+        choice1: "Heart Reaper",
+        choice2: "Shadowlily",
+        choice3: "Searing Black Lotus",
+        choice4: "Without a Cure",
+        answer: 4
+    },
+    {
+        question: "What was Mo Ran's title as Emperor?",
+        choice1: "Taxian-Jun",
+        choice2: "Yuheng Elder",
+        choice3: "Hanguang-Jun",
+        choice4: "Tanlang-Jun",
+        answer: 1
+    },
+    {
+        question: "What type of spiritual magic is Chu Wanning the most proficient at?",
+        choice1: "Fire elements",
+        choice2: "Healing",
+        choice3: "Barriers",
+        choice4: "Illusions",
+        answer: 3
+    },
+];
+
+const correctPoint = 10;
