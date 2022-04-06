fdba3b0ab8949394f2dbbfcc724e41d78264e42e
Akuru <anjalismith0529@gmail.com>
2022-04-05 23:03:02 -0400

    removed example folder

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # quiz_game
-creating a simple quiz game 
+creating a simple quiz game based off of Danmei, use index.html to begin the game.
--- assets/gameOver.js
+++ assets/gameOver.js
@@ -1,13 +1,23 @@
 var username = document.querySelector("#username");
 var save = document.querySelector(".saveScore");
 var recentScore = localStorage.getItem("recentScore");
-var finalScore = document.getElementById("highscore-text")
+var finalScore = document.getElementById("highscore-text");
+var highscore = JSON.parse(localStorage.getItem("highscore")) || [];
 
 //when you click the save score button your score should be saved
-function saveFinalScore (event) {
+saveFinalScore = () => {
     finalScore.textContent = recentScore;
-    event.preventDefault();
-}
+    
+    var score = {
+        score: recentScore,
+        name: username.value
+    };
+    highscore.push(score);
+localStorage.setItem("highscore", JSON.stringify(highscore));
 
+};
 saveFinalScore()
-var totalScores = JSON.parse(localStorage.getItem("recentScore"));
+
+
+
+
